6ba0dc1e7d9f041802a2de5b41865eafe0cbd606
u3484 <u3484@192.168.86.109>
2022-07-12 21:23:32 +0800

    update

---
+++ src/main/java/edu/up/controller/main.java
@@ -0,0 +1,13 @@
+package edu.up.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+@Controller
+public class main {
+	@RequestMapping("/search")
+	public void search(@RequestParam("string")String str) {
+		System.out.println(str);
+	}
+}
---
+++ src/main/java/edu/up/controller/newcl.java
@@ -0,0 +1,12 @@
+package edu.up.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class newcl {
+	@RequestMapping("/help")
+	public void help() {
+		System.out.println("help!");
+	}
+}
